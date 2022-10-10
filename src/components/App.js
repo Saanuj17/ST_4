61564d6c909fa7c2959115c9be4f1801d36e437f
Saanuj17 <110155556+Saanuj17@users.noreply.github.com>
2022-10-10 23:47:21 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+import Nav from './Nav/Nav';
+import Cart from './Cart/Cart';
+
+import router from '../router.js'
+
+export default function App() {
+  return (
+    <div>
+      <Nav />
+      <div id="App__mainContainer">
+        <div id="App__routerContainer">
+          {router}
+          <Cart />
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+
+import './Nav.css';
+
+import ShoppingCart from 'react-icons/lib/fa/shopping-cart';
+
+
+
+function Nav({ items }) {
+  return (
+    <div id="Nav__container">
+      <div id="Nav__linksContainer">
+        <Link to="/" className="navLink"><span className="Nav__label">Store</span></Link>
+
+        <Link to="/checkout" className="navLink"><div id="Nav__checkoutContainer">
+          <div id="Nav__itemsInCart"> {items} </div>
+          <ShoppingCart id="Nav__shoppingCart" />
+        </div>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    items: state.cart.length
+  };
+}
+
+export default connect(mapStateToProps)(Nav);
